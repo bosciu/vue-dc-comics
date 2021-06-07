81d7a02917b15e7e203dd41ad811de07640af702
bosciu <bosciu@icloud.com>
2021-06-07 14:46:25 +0200

    Creazione struttura macroaree

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+	<footer>Contenuto Footer</footer>
+</template>
+
+<script>
+export default {
+	name: "Footer"
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+	<main>Contenuto Main</main>
+</template>
+
+<script>
+export default {
+	name: "Main"
+};
+</script>
+
+<style scoped></style>
