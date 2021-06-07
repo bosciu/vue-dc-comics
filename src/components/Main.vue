c90cd7243ba7d06b7b3468170cc261543c210903
bosciu <bosciu@icloud.com>
2021-06-07 19:08:48 +0200

    Completamento bonus

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,42 @@
 		<div class="container">
 			<h1>-->Content goes here &lt; --</h1>
 		</div>
+		<div class="stores">
+			<div class="container">
+				<div class="store-link">
+					<img
+						src="../assets/img/buy-comics-digital-comics.png"
+						alt=""
+					/>
+					<span>Digital comics</span>
+				</div>
+				<div class="store-link">
+					<img
+						src="../assets/img/buy-comics-merchandise.png"
+						alt=""
+					/>
+					<span>DC Merchandise</span>
+				</div>
+				<div class="store-link">
+					<img
+						src="../assets/img/buy-comics-subscriptions.png"
+						alt=""
+					/>
+					<span>Subscription</span>
+				</div>
+				<div class="store-link">
+					<img
+						src="../assets/img/buy-comics-shop-locator.png"
+						alt=""
+					/>
+					<span>Comic shop locator</span>
+				</div>
+				<div class="store-link">
+					<img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+					<span>DC power visa</span>
+				</div>
+			</div>
+		</div>
 	</main>
 </template>
 
@@ -13,12 +49,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variables";
 main {
-	height: 150px;
 	background: black;
 	color: white;
 	h1 {
 		line-height: 150px;
+	}
+	.stores {
+		height: 150px;
+		background-color: $brandColor;
+		.container {
+			display: flex;
+			justify-content: space-around;
+			height: 100%;
+			.store-link {
+				display: flex;
+				align-items: center;
+				width: 20%;
+				&:last-child > img {
+					height: 25%;
+				}
+				img {
+					height: 35%;
+					margin: 0 8%;
+				}
+				span {
+					font-size: 14px;
+					text-transform: uppercase;
+				}
+			}
+		}
 	}
 }
 </style>
