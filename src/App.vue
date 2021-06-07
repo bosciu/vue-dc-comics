a72111057aaef16bed17f7cfbb26a3b3a74f1d64
bosciu <bosciu@icloud.com>
2021-06-07 16:43:04 +0200

    Completamento Header

--- src/App.vue
+++ src/App.vue
@@ -21,4 +21,6 @@ export default {
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import "./style/generic-style";
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,23 @@
 <template>
-	<header>Contenuto Header</header>
+	<header class="container">
+		<div id="logo-container">
+			<img src="../assets/img/dc-logo.png" alt="" />
+		</div>
+		<nav>
+			<ul>
+				<li><a href="#">Characters</a></li>
+				<li class="active"><a href="#">Comics</a></li>
+				<li><a href="#">Movies</a></li>
+				<li><a href="#">Tv</a></li>
+				<li><a href="#">Games</a></li>
+				<li><a href="#">Collectibles</a></li>
+				<li><a href="#">Videos</a></li>
+				<li><a href="#">Fans</a></li>
+				<li><a href="#">News</a></li>
+				<li><a href="#">Shop</a></li>
+			</ul>
+		</nav>
+	</header>
 </template>
 
 <script>
@@ -8,4 +26,43 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import "../style/variables";
+.container {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	overflow: hidden;
+	img {
+		padding: 10% 0;
+	}
+	nav {
+		flex-grow: 1;
+		ul {
+			display: flex;
+			justify-content: flex-end;
+			li {
+				margin-left: 3%;
+				font-size: 14px;
+				font-weight: 600;
+				text-transform: uppercase;
+				cursor: pointer;
+				color: #464646;
+				&.active {
+					position: relative;
+					color: $brandColor;
+				}
+				&.active::after {
+					content: "";
+					position: absolute;
+					top: 65px;
+					width: 100%;
+					height: 20px;
+					display: block;
+					background: $brandColor;
+				}
+			}
+		}
+	}
+}
+</style>
