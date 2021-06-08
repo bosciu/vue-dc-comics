05f4846e00025d4085acfa16b6cf05cc2ab12237
bosciu <bosciu@icloud.com>
2021-06-08 16:33:11 +0200

    Completamento esercizio con component Button

--- src/components/Comics.vue
+++ src/components/Comics.vue
@@ -1,18 +1,31 @@
 <template>
 	<div class="comics-container">
 		<div class="container">
-			<div class="card" v-for="(card, index) in cards" :key="index"></div>
+			<div id="label">Current series</div>
+			<Card
+				v-for="(card, index) in cardsList"
+				:key="index"
+				:img-src="card.thumb"
+				:series="card.series"
+			/>
+			<Button text="Load More" />
 		</div>
 	</div>
 </template>
 
 <script>
-import cards from "../../JSON/dc-comics";
+import cardsList from "../../JSON/dc-comics";
+import Card from "./Card";
+import Button from "./../generic-components/Button";
 export default {
 	name: "Comics",
+	components: {
+		Card,
+		Button
+	},
 	data: function () {
 		return {
-			cards
+			cardsList
 		};
 	}
 };
@@ -20,17 +33,23 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style/mixins";
+@import "../style/variables";
 .comics-container {
 	background-color: #1c1c1c;
 	& > .container {
 		@include flex-center("both");
 		flex-wrap: wrap;
-		.card {
-			width: calc(100% / 6);
-			height: 200px;
-			margin: 3% 0;
-			background-color: yellow;
-			border: 3px solid red;
+		position: relative;
+		padding-bottom: 1%;
+		#label {
+			position: absolute;
+			top: -4%;
+			left: 0;
+			padding: 12px 10px;
+			background-color: $brandColor;
+			text-transform: uppercase;
+			font-weight: 700;
+			font-size: 24px;
 		}
 	}
 }
